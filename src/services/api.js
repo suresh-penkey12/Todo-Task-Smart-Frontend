63f4ac37b7d2fd70999f75110b213141fb99b99a
Suresh Penkey <118256832+suresh-penkey12@users.noreply.github.com>
2025-07-08 16:18:38 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://todo-task-smart-backend-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
